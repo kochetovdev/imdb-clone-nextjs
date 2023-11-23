2be075255e36d2d1d281597669f4a50abb837b5d
Kochetov <kochetov.yevhen.dev@gmail.com>
2023-11-23 16:36:08 +0200

    create the header section

---
+++ app/components/Header.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import MenuItem from "./MenuItem";
+import { IoIosHome } from "react-icons/io";
+import { IoInformationCircleSharp } from "react-icons/io5";
+
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center mx-2 py-6 max-w-6xl sm:mx-auto">
+      <div className="flex">
+        <MenuItem title="HOME" address="/" Icon={IoIosHome} />
+        <MenuItem
+          title="ABOUT"
+          address="/about"
+          Icon={IoInformationCircleSharp}
+        />
+      </div>
+      <div>
+        <Link href="/">
+          <h2 className="text-2xl">
+            <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">IMDB</span>
+            <span className="text-xl hidden sm:inline">Clone</span>
+          </h2>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
